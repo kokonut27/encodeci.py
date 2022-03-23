f79ac1bbec0b63e26ae4d721287e7327e306d2bc
kokomi simp :3 <beol0127@gmail.com>
2022-03-23 20:41:40 +0000

    chore: setup config files + base code

--- encodeci/main.py
+++ encodeci/main.py
@@ -1,2 +1,13 @@
+import random
 import os
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x' , 'y', 'z']
+
+class encode:
+  def __init__(self, string):
+    self.string = str(string).lower()
+
+  def encode(self):
+    return self.string
+
+encode("hiyo").encode()
